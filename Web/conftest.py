66d727daed757e99df30f1e5a0ca995547471a4c
Grigorii Nevskii <131486470+Grusha446@users.noreply.github.com>
2024-03-17 19:56:12 +0300

    Add files via upload

---
+++ Web/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+def driver(request):
+    browser = request.config.getoption("--browser")
+    if browser == "chrome":
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+    elif browser == "firefox":
+        service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service)
+    else:
+        raise ValueError(f"Unsupported browser: {browser}")
+
+    def fin():
+        driver.quit()
